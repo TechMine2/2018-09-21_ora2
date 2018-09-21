41e818e3d1b6a0e51e3bbea8fcb3e49fbc7a8882
Petrik Lajos Szg <info@petrik.hu>
2018-09-21 15:36:50 +0200

    Legkisebb elem keresése

--- GitConsoleApp01/Program.cs
+++ GitConsoleApp01/Program.cs
@@ -11,21 +11,33 @@ namespace GitConsoleApp01
         static void Main(string[] args)
         {
             int[] tomb = new int[10];
-            int max=0;
+            int max = 0;
+            int min = 0;
             int db;
             Console.WriteLine("Hány darab számot szeretne beírni?");
             db = Convert.ToInt32(Console.ReadLine());
+            bool asd = false;
 
             for (int i = 0; i < db; i++)
             {
-                Console.WriteLine("Írjon be még {0} számot!", + db-i);
+                Console.WriteLine("Írjon be még {0} számot!", +db - i);
                 tomb[i] = Convert.ToInt32(Console.ReadLine());
-                if (tomb[i]>max)
+                if (asd == false)
+                {
+                    min = tomb[i];
+                    max = tomb[i];
+                    asd = true;
+                }
+                if (tomb[i] > max)
                 {
                     max = tomb[i];
                 }
+                if (tomb[i] < min)
+                {
+                    min = tomb[i];
+                }
             }
-            Console.WriteLine("Legnagyobb szám: {0}", max);
+            Console.WriteLine("Legnagyobb szám: {0}\nLegkisebb szám: {1}", max, min);
             Console.ReadKey();
         }
     }
