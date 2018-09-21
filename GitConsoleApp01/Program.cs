00f1bfe7ec56c078662cfa5fd54bb1d1156fe9a9
Petrik Lajos Szg <info@petrik.hu>
2018-09-21 15:46:24 +0200

    Hibaüzenet, ha nem szám lett beütve

--- GitConsoleApp01/Program.cs
+++ GitConsoleApp01/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace GitConsoleApp01
 {
@@ -13,15 +14,37 @@ namespace GitConsoleApp01
             int[] tomb = new int[10];
             int max = 0;
             int min = 0;
-            int db;
-            Console.WriteLine("Hány darab számot szeretne beírni?");
-            db = Convert.ToInt32(Console.ReadLine());
+            int db = 10;
             bool asd = false;
+            Console.WriteLine("Hány darab számot szeretne beírni?");
+            try
+            {
+                db = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Nem szám lett megadva!");
+                Thread.Sleep(2000);
+                return;
+            }
+
+
 
             for (int i = 0; i < db; i++)
             {
                 Console.WriteLine("Írjon be még {0} számot!", +db - i);
-                tomb[i] = Convert.ToInt32(Console.ReadLine());
+                try
+                {
+                    tomb[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Nem szám lett megadva!");
+                    Thread.Sleep(2000);
+                    return;
+                }
+
+
                 if (asd == false)
                 {
                     min = tomb[i];
