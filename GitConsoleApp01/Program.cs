84dc6093ee6c7afebd4ac50f508f762edf493ab0
Petrik Lajos Szg <info@petrik.hu>
2018-09-21 15:31:42 +0200

    Darabszám bekérés

--- GitConsoleApp01/Program.cs
+++ GitConsoleApp01/Program.cs
@@ -12,9 +12,13 @@ namespace GitConsoleApp01
         {
             int[] tomb = new int[10];
             int max=0;
-            for (int i = 0; i < 10; i++)
+            int db;
+            Console.WriteLine("Hány darab számot szeretne beírni?");
+            db = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < db; i++)
             {
-                Console.WriteLine("Írjon be még {0} számot!", + 10-i);
+                Console.WriteLine("Írjon be még {0} számot!", + db-i);
                 tomb[i] = Convert.ToInt32(Console.ReadLine());
                 if (tomb[i]>max)
                 {
